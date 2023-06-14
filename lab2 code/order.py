13226c72ec276ff3e4a816cdd06c270f4f9eb00c
Lyubov Moskvina <63208434+lyumos@users.noreply.github.com>
2023-06-14 13:30:30 +0300

    Create order.py

---
+++ lab2 code/order.py
@@ -0,0 +1,16 @@
+import customer
+import supplier
+import lab1
+import lab2
+
+num = 50050050
+date = '08.05.2020'
+mobile_product = 'Телефония'
+traffic_product = 'Интернет'
+bill_num = 15
+bill_date = '08.05.2020'
+leader = 'Грущинская Т.В.'
+accountant = 'Орлова А.П.'
+mobile_price = lab1.mobile_bill()
+traffic_price = lab2.traffic_bill()
+amount = mobile_price + traffic_price
